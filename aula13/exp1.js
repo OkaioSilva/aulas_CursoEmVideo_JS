bd2bbe46a5956cd642b436e2f5d4d179f369fc32
Kaio silva <118384069+OkaioSilva@users.noreply.github.com>
2024-05-10 15:19:18 -0300

    aula 13

---
+++ aula13/exp1.js
@@ -0,0 +1,5 @@
+var C = 1;
+while(C <= 5){
+    console.log('funcionou o loop')
+    C++
+}
---
+++ aula13/exp2.js
@@ -0,0 +1,5 @@
+console.log("Vai começar!")
+for(i = 1; i <= 10; i++){
+    console.log(`passo ${i}`);
+}
+console.log("Fim!")
